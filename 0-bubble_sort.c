13f9700c5e193ef680998eb63c25717bfe4d0923
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-21 21:52:12 +0100

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,20 +11,19 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int tmp, swap;
 
-	if (size < 2)
-		return;
-
-	for (i = 0; i < size; i++)
+	for (i = 0, swap = 1; i < size && swap; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		swap = 0;
+		for (j = 0; (j + 1) < size - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				swap = 1;
 				print_array(array, size);
 			}
 		}
